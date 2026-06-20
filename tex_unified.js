5ebf4e8d635832c73275bc28cb39e24fec1a4ed4
cl0udsw1tch <nurein.umeya@mail.utoronto.ca>
2026-06-20 16:08:34 -0400

    route error fix

--- tex_unified.js
+++ tex_unified.js
@@ -12,11 +12,12 @@ import path from 'path'
 import fs, { existsSync } from "node:fs"
 import { exists } from "node:fs"
 
+console.log(process.env)
 const paper = process.env.npm_config_paper;
 
 if (!paper) {
-    console.log("TEX-PIPELINE: Not rendering paper.")
-    process.exit(0)
+  console.log("TEX-PIPELINE: Not rendering paper.")
+  process.exit(0)
 }
 
 console.log("TEX-PIPELINE: paper renderered=" + paper)
@@ -28,23 +29,23 @@ const output_dir = path.join(process.cwd(), "public", paper)
 const output_path = path.join(output_dir.toString(), "index.html")
 
 if (!existsSync(output_dir)) {
-    try {
-        fs.mkdirSync(output_dir)
-    } catch (e) {
-        console.log("Error creating output folder: " + e.toString())
-        process.exit(1)
-    }
+  try {
+    fs.mkdirSync(output_dir)
+  } catch (e) {
+    console.log("Error creating output folder: " + e.toString())
+    process.exit(1)
+  }
 }
 
 const file = await unified()
-    .use(rehypeParse, { fragment: true })
-    .use(rehypeEasytex)
-    .use(rehypeKatex)
-    // .use(rehypeDocument, {
-    //     css: 'https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css'
-    // })
-    .use(rehypeStringify, { fragment: true })
-    .process(await read(input_path))
+  .use(rehypeParse, { fragment: true })
+  .use(rehypeEasytex)
+  .use(rehypeKatex)
+  // .use(rehypeDocument, {
+  //     css: 'https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css'
+  // })
+  .use(rehypeStringify, { fragment: true })
+  .process(await read(input_path))
 
 file.path = output_path
 await write(file)
