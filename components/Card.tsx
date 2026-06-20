057f763c8ef9a9ea258e02e620ac181e8119258a
cl0udsw1tch <nurein.umeya@mail.utoronto.ca>
2026-06-20 15:30:45 -0400

    coloring, card stuff

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 import MetaData from "@/components/MetaData";
 
 export default function Card<T extends MetaData>({ metaData }: { metaData: T }) {
-    return (
-        <Link href={`/${metaData.class}/${metaData.slug}`} className="list-item-card">
-            <div className="list-item-meta">
-                <span className="list-item-category">{metaData.category}</span>
-                <span className="list-item-date">{metaData.date}</span>
-            </div>
+  return (
+    <Link href={`/${metaData.class}/${metaData.slug}`} className="list-item-card">
+      <div className="list-item-meta">
+        <span className="list-item-category">{metaData.category}</span>
+        <span className="list-item-date">{metaData.date}</span>
+      </div>
 
-            <h3>{metaData.title}</h3>
-            <p>{metaData.description}</p>
-        </Link>
-    );
+      <h3 className="list-item-title">{metaData.title}</h3>
+      <p className="list-item-desc">{metaData.description}</p>
+    </Link>
+  );
 }
