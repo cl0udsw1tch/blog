73fc3ec3ce74885a86d5b588a552e519ab67a2aa
cl0udsw1tch <nurein.umeya@mail.utoronto.ca>
2026-06-20 13:40:10 -0400

    fixed scrolling on leetcode

--- app/leetcode/[slug]/CodeClient.tsx
+++ app/leetcode/[slug]/CodeClient.tsx
@@ -10,40 +10,41 @@ import Markdown from 'react-markdown';
 
 
 const ext2lang: { [key: string]: string } = {
-	'py': 'python'
+  'py': 'python'
 }
 export default function CodeClient({ problem, codeBlock, codeDesc }: { problem: LeetCodeData, codeBlock: string, codeDesc: string }) {
-	useEffect(() => {
-		// This executes exactly once when the component mounts on the screen
-		Prism.highlightAll();
-	}, []);
+  useEffect(() => {
+    // This executes exactly once when the component mounts on the screen
+    Prism.highlightAll();
+  }, []);
 
-	return (
-		<div className="problem page-content">
-			<aside >
-				<div className="meta">
-					<h1>{problem.title}</h1>
+  return (
+    <div className="problem page-content">
+      <aside >
+        <div className="meta">
+          <h1>{problem.title}</h1>
 
-					<div className="problem page-details">
-						<span>{problem.category}</span> · <span>{problem.date}</span>
-					</div>
+          <div className="problem page-details">
+            <span>{problem.category}</span> · <span>{problem.date}</span>
+          </div>
 
-					<div dangerouslySetInnerHTML={{ __html: codeDesc }} />
 
-				</div>
-			</aside>
-			<article>
-				<h1 className="text-4xl">Problem: {problem.title}</h1>
-				<pre>
-					<code className={"font-mono w-0.5" + " language-" + ext2lang[problem.type]}>
-						{codeBlock}
-					</code>
+        </div>
 
-				</pre>
+        <div className='codeDesc' dangerouslySetInnerHTML={{ __html: codeDesc }} />
+      </aside>
+      <article>
+        <h1 className="text-4xl">Problem: {problem.title}</h1>
+        <pre>
+          <code className={"font-mono w-0.5" + " language-" + ext2lang[problem.type]}>
+            {codeBlock}
+          </code>
 
-			</article>
-		</div>
+        </pre>
 
-	);
+      </article>
+    </div>
+
+  );
 }
 
